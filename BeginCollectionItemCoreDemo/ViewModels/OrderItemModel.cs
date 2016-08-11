16b7fb8e3cdb06000211e11eeaafa9d0af7c5447
Saad Farooq <saad749@yahoo.com>
2016-08-11 13:51:12 +0300

    initial commit

---
+++ BeginCollectionItemCoreDemo/ViewModels/OrderItemModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeginCollectionItemCoreDemo.ViewModels
+{
+    public class OrderItemModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public double Quantity { get; set; }
+
+
+    }
+}
---
+++ BeginCollectionItemCoreDemo/ViewModels/OrderModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeginCollectionItemCoreDemo.ViewModels
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public DateTimeOffset OrderDateTime { get; set; }
+        public List<OrderItemModel> OrderItems { get; set; }
+
+    }
+}
