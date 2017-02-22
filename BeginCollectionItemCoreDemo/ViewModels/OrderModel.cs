8d8f692996bd118f2b25bb136c24420f1e1e8045
Matteo Pozzani <m.phobos@hotmail.com>
2017-02-22 09:00:56 +0100

    Nested Collection Demo

--- BeginCollectionItemCoreDemo/ViewModels/OrderModel.cs
+++ BeginCollectionItemCoreDemo/ViewModels/OrderModel.cs
@@ -10,7 +10,7 @@ namespace BeginCollectionItemCoreDemo.ViewModels
         public int Id { get; set; }
         public int CustomerId { get; set; }
         public DateTimeOffset OrderDateTime { get; set; }
-        public List<OrderItemModel> OrderItems { get; set; }
+        public List<OrderItemModel> OrderItems { get; set; } = new List<OrderItemModel>();
 
     }
 }
